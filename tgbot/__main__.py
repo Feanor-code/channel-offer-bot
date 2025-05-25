5c46c804d0ceb66b7b5553dab75419ef5d88e528
Feanor-code <admin@sower.space>
2025-05-25 21:56:37 +0000

    Удалил ненужное говно

--- tgbot/__main__.py
+++ tgbot/__main__.py
@@ -5,19 +5,11 @@ import logging
 from tgbot.handlers.edit_post import router
 from .dispatcher import bot, dp
 
-logging = logging.getLogger(__name__)
-
-
-async def on_startup() -> None:
-    pass
-    
 
 async def main() -> None:
     dp.include_routers(router)
-    dp.startup.register(on_startup)
-
-    # await bot.delete_webhook(drop_pending_updates=True) :: наверное, лучше включить
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    # await bot.delete_webhook(drop_pending_updates=True) :: наверное, лучше включить
 
 
 if __name__ == "__main__":
